9df826cd1cf30f66b9e35a25aad0a92acf82efe9
EphremHA <ephrem.24@gmail.com>
2022-06-30 06:41:23 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * * Printing Function
+ */
 int main(void)
 {
-	char sen[100] = "Programming is like building a multilingual puzzle\n";
+	char sen[100] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(sen);
 	return (0);
 }
+/**
+ *Positive return
+ */
