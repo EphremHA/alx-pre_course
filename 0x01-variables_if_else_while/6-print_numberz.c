cdd59eea3d480c7e522cd69049822f2ebb6aaf69
EphremHA <ephrem.24@gmail.com>
2022-07-01 09:06:19 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (n = 48; n < 58; ++n)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
