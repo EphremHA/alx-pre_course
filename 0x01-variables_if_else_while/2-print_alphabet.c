3bd0d58c011555a02ac0cf6f015248425d96b6b7
EphremHA <ephrem.24@gmail.com>
2022-07-01 07:57:56 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+	int z = 122;
+
+	for (a >= 97; a <= 122; ++a)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
